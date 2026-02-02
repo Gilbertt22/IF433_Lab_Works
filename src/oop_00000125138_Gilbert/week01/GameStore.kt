121708acd649e483548bbc4393fe4a239c86452d
Gilbert <gilbert2u@student.umn.ac.id>
2026-02-02 16:47:32 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000125138_Gilbert/week01/GameStore.kt
+++ src/oop_00000125138_Gilbert/week01/GameStore.kt
@@ -1,7 +1,12 @@
 package oop_00000125138_Gilbert.week01
 
 fun main() {
-    // Langkah 1: Persiapan Data
     val gameTitle: String = "Resident Evil 4 Remake"
     val price: Int = 600000
+
+    // Memanggil fungsi discount (untuk testing sementara)
+    val discount = calculateDiscount(price)
 }
+
+// Langkah 2: Fungsi calculateDiscount menggunakan Expression Body
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
