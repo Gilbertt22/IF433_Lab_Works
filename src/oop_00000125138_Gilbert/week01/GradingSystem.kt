7ad8379194a894d8e21d4f941892d9fb35cedd83
Gilbert <gilbert2u@student.umn.ac.id>
2026-02-02 15:45:01 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000125138_Gilbert/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000125138_Gilbert.week01// Pastikan package sesuai
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+// Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
