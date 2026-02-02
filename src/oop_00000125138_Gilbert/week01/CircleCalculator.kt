d4473d0a9f80983ef5f054fd57920975a8cc5f40
Gilbert <gilbert2u@student.umn.ac.id>
2026-02-02 16:43:20 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_00000125138_Gilbert/week01/CircleCalculator.kt
+++ src/oop_00000125138_Gilbert/week01/CircleCalculator.kt
@@ -2,23 +2,18 @@ package oop_00000125138_Gilbert.week01
 
 fun main(args: Array<String>) {
     // Variable definition
-    val radius:  = 7.0
-    val pi:  = 3.14
+    val radius = 7.0
+    val pi = 3.14
 
     // Calculation
-    val area: = pi * radius * radius
+    val area = pi * radius * radius
 
     // Output Concatenation
     println("Radius: $radius, Area: $area")
 
-    // Logic check
-    checkSize(area)
+    // Langkah 3: Memanggil fungsi checkSize di dalam println
+    println(checkSize(area))
 }
 
-fun checkSize(area) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+// Langkah 3: Mengubah checkSize menjadi Expression Body Function
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
