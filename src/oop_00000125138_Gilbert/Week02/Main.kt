8bc5aa30423a4dc65f322b7807a91090d6e2b536
Gilbert <gilbert2@student.umn.ac.id>
2026-02-09 16:29:02 +0700

    week02: implement selection logic for constructors

--- src/oop_00000125138_Gilbert/Week02/Main.kt
+++ src/oop_00000125138_Gilbert/Week02/Main.kt
@@ -5,23 +5,34 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("--- Pendaftaran Mahasiswa Baru ---")
+    println("--- APLIKASI PMB UMN ---")
     print("Masukkan Nama: ")
     val name = scanner.nextLine()
 
-    print("Masukkan NIM (5 karakter): ")
+    print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.nextLine()
 
-    // Validasi di sisi pemanggil (Main) sesuai Checkpoint 3
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+            println("Status: Pendaftaran Selesai.")
+
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+            println("Status: Pendaftaran Selesai.")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
