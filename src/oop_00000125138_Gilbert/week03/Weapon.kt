74b75c9cdcb59b0d3d14360151cf5babae735f76
Gilbert <gilbert2@student.umn.ac.id>
2026-02-17 08:15:28 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000125138_Gilbert/week03/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_00000125138_Gilbert.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai tetap $field.")
+            } else if (value > 1000) {
+                println("Peringatan: Damage Overpowered! Dipaksa ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
