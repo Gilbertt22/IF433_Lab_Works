60fa4b2a07e3e4259ca791075b20aaa125399c39
Gilbert <gilbert2@student.umn.ac.id>
2026-02-23 16:09:27 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000125138_Gilbert/week04/Main.kt
+++ src/oop_00000125138_Gilbert/week04/Main.kt
@@ -8,29 +8,22 @@ fun main() {
 
     println("\n--- Testing Car ---")
     val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()     // Memanggil method milik sendiri
-    myCar.honk()          // Memanggil method Parent
-    myCar.accelerate()     // Memanggil method Parent
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
 
     println("\n--- Testing Electric Car ---")
     val myTesla = ElectricCar(brand = "Tesla", numberOfDoors = 2, batteryCapacity = 85)
-
     myTesla.accelerate()
     myTesla.honk()
-    myTesla.openTrunk()
-    // Testing Tugas Mandiri 2
-    println("\n--- Testing Employee & Manager ---")
-    val emp = Employee("Budi", 5000)
-    emp.work()
-    println("Bonus Budi: ${emp.calculateBonus()}")
 
-    val mgr = Manager("Gilbert", 8000, 5)
+    println("\n--- Testing Tugas Mandiri 2 ---")
+
+    val mgr = Manager("Gilbert Manager", 8000000, 5)
     mgr.work()
-    println("Bonus Manager ${mgr.name}: ${mgr.calculateBonus()}")
-    // Lanjutan
-    println("\n--- Testing Developer ---")
-    val dev = Developer("Gilbert Dev", 7000000, "Kotlin")
-    dev.work()
+    println("Bonus Manager ${mgr.name}: Rp ${mgr.calculateBonus()}")
 
+    val dev = Developer("Gilbert Developer", 7000000, "Kotlin")
+    dev.work()
     println("Bonus Developer ${dev.name}: Rp ${dev.calculateBonus()}")
 }
