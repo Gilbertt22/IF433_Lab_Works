625e1384ecd50fa78a841935544427dddc23330e
Gilbert <gilbert2@student.umn.ac.id>
2026-03-09 21:35:30 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000125138_Gilbert/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000125138_Gilbert.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
