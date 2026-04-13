1736b93a36582c80bb15d66698730e0bfd4fd428
Gilbert <gilbert2@student.umn.ac.id>
2026-04-13 15:33:30 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000125138_Gilbert/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000125138_Gilbert.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
