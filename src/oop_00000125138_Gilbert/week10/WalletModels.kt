c527e5d96611b7b58da9b4adbb261adbdc86030a
Gilbert <gilbert2@student.umn.ac.id>
2026-04-27 14:45:34 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000125138_Gilbert/week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000125138_Gilbert.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
