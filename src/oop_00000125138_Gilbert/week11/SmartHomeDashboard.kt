bf94e0dc9945a6a1367f3c2d18b02e6b1d27d3f8
Gilbert <gilbert2@student.umn.ac.id>
2026-05-04 16:09:30 +0700

    week11: (task) implement device search with let

--- src/oop_00000125138_Gilbert/week11/SmartHomeDashboard.kt
+++ src/oop_00000125138_Gilbert/week11/SmartHomeDashboard.kt
@@ -24,4 +24,10 @@ fun main() {
     homeDevices.add(acInverter)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+    val searchResult = homeDevices.find { it.category == "Camera" }
+
+    searchResult?.let {
+        val diagnosticInfo = it.diagnose()
+        println(diagnosticInfo)
+    }
 }
